d85ed1fbef5ee75b8ed00b22f68fbec4bc49439c
snasirh <snasirh09@gmail.com>
2023-09-27 02:42:12 -0400

    Added Test output ss

--- test.java
+++ test.java
@@ -4,9 +4,11 @@ public class test{
         GroceryProductFactory factory1 = factoryProducer.getFactory("apples");
         factory1.setPrice(3.99);
         System.out.println(factory1);
+
         GroceryProductFactory factory2 = factoryProducer.getFactory("bananas");
         factory2.setPrice(4.99);
         System.out.println(factory2);
+        
         GroceryProductFactory factory3 = factoryProducer.getFactory("FalseValue");
     }
 }
