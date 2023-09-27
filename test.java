33612635d3082bb85ee35ba1068169c164d7055d
snasirh <snasirh09@gmail.com>
2023-09-27 14:48:43 -0400

    Added screen dumps | Completed Driver

--- test.java
+++ test.java
@@ -1,14 +1,37 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class test{
 
     public static void main(String[] args) {
-        GroceryProductFactory factory1 = factoryProducer.getFactory("apples");
-        factory1.setPrice(3.99);
-        System.out.println(factory1);
+              try {
+            // Read and Scan file
+            File dataFile = new File("./src/data/data.txt");
+            Scanner read = new Scanner(dataFile);
 
-        GroceryProductFactory factory2 = factoryProducer.getFactory("bananas");
-        factory2.setPrice(4.99);
-        System.out.println(factory2);
-        
-        GroceryProductFactory factory3 = factoryProducer.getFactory("FalseValue");
+            while (read.hasNextLine()) {
+                String data = read.nextLine();
+
+                // Split Product name and price
+                String[] parts = data.split(" ");
+
+                String product = parts[0];
+                double price = Double.parseDouble(parts[1]);
+
+                // Get the factory for the product
+                GroceryProductFactory factory = factoryProducer.getFactory(product);
+                if (factory != null) {
+                    // Set the price and print the factory
+                    factory.setPrice(price);
+                    System.out.println(factory);
+                }
+            }
+        read.close();    
+        } catch (FileNotFoundException e) {
+            System.out.println("Sonething went wrong -_-");
+            e.printStackTrace();
+        }
+
     }
 }
